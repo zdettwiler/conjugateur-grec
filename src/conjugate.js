9d3c1d15948792517e7e40c2bc65e94c04664fd8
Zach Dettwiler <z.dettwiler@gmail.com>
2020-12-13 16:06:47 +0000

    better structure of function

--- src/conjugate.js
+++ src/conjugate.js
@@ -1,14 +1,67 @@
 import { activeIndicativePresent } from './activeIndicative'
+import desinences from './desinences'
+import getStem from './getStem'
+import { arrayToConjugationPattern } from './utils'
 
+const VOICE_ACTIVE = 'VOICE_ACTIVE'
+const MOOD_INDICATIVE = 'indicatif'
+const TENSE_PRESENT = 'present'
+const TENSE_FUTURE = 'futur'
 
 export default function conjugate(verb) {
   return {
     active: {
       indicative: {
-        present: activeIndicativePresent(verb)
+        // present: activeIndicativePresent(verb)
+        present: conjugator(verb, VOICE_ACTIVE, MOOD_INDICATIVE, TENSE_PRESENT),
+        future: conjugator(verb, VOICE_ACTIVE, MOOD_INDICATIVE, TENSE_FUTURE),
+
       }
     }
   }
 }
 
 // conjugate(verb).voice.mood.tense
+
+function contraction(stem, desinence) {
+  let contractions = {
+    ε: {
+      'ει': 'ει',
+      'ου': 'ου',
+      'ε': 'ει',
+      'ο': 'ου',
+      'ω': 'ω'
+    }
+  }
+
+  let lastStemChar = stem[stem.length-1]
+
+  if (Object.keys(contractions).includes(lastStemChar)) {
+    for (let beginEnding of Object.keys(contractions[lastStemChar])) {
+      // if desinence starts with contractable
+      if (desinence.indexOf(beginEnding) === 0) {
+        return stem.slice(0, -1) // remove character from end of stem
+          + contractions[lastStemChar][beginEnding] // add output of contraction
+          + desinence.slice(beginEnding.length) // remove chars from beginning of desinence
+      }
+    }
+
+  } else { // no contraction is done
+    return stem + desinence
+  }
+}
+
+function conjugator(verb, voice, mood, tense) {
+  let verbInfo = getStem(verb)
+  let conjugation = []
+
+  if (verbInfo.type === '-ω') {
+    const conjugation_settings = desinences[mood][tense]
+    conjugation = conjugation_settings.endings.map(ending => {
+      return contraction(verbInfo.stem + conjugation_settings.suffix, ending)
+    })
+  }
+
+  return arrayToConjugationPattern(conjugation)
+
+}
--- test/indicative.test.js
+++ test/indicative.test.js
@@ -1,8 +1,12 @@
 import { activeIndicativePresent } from '../src/activeindicative'
+import conjugate from '../src/conjugate'
+
+const luo = conjugate('λυω')
+const phileo = conjugate('φιλεω')
 
 describe('indicativePresent()', () => {
   it('should conjugate λυω properly', () => {
-    expect(activeIndicativePresent('λυω')).toEqual({
+    expect(luo.active.indicative.present).toEqual({
       sg: {
         1: 'λυω',
         2: 'λυεις',
@@ -17,7 +21,7 @@ describe('indicativePresent()', () => {
   })
 
   it('should conjugate φιλεω properly', () => {
-    expect(activeIndicativePresent('φιλεω')).toEqual({
+    expect(phileo.active.indicative.present).toEqual({
       sg: {
         1: 'φιλω',
         2: 'φιλεις',
@@ -32,19 +36,34 @@ describe('indicativePresent()', () => {
   })
 })
 
-// describe('indicativeFuture()', () => {
-//   it('should conjugate λυω properly', () => {
-//     expect(indicativeFuture('λυω')).toEqual({
-//       sg: {
-//         1: 'λυω',
-//         2: 'λυεις',
-//         3: 'λυει'
-//       },
-//       pl: {
-//         1: 'λυομεν',
-//         2: 'λυετε',
-//         3: 'λυουσι(ν)'
-//       }
-//     })
-//   })
-// })
+describe('indicativeFuture()', () => {
+  it('should conjugate λυω properly', () => {
+    expect(luo.active.indicative.future).toEqual({
+      sg: {
+        1: 'λυσω',
+        2: 'λυσεις',
+        3: 'λυσει'
+      },
+      pl: {
+        1: 'λυσομεν',
+        2: 'λυσετε',
+        3: 'λυσουσι(ν)'
+      }
+    })
+  })
+
+  it('should conjugate φιλεω properly', () => {
+    expect(phileo.active.indicative.future).toEqual({
+      sg: {
+        1: 'φιλεσω',
+        2: 'φιλεσεις',
+        3: 'φιλεσει'
+      },
+      pl: {
+        1: 'φιλεσομεν',
+        2: 'φιλεσετε',
+        3: 'φιλεσουσι(ν)'
+      }
+    })
+  })
+})
